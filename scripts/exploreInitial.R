c8856d5e28382621749d0b13bf8d7c24b2ade3cb
Ben Anderson <dataknut@icloud.com>
2019-10-15 16:06:07 +1300

    added code to test & explore sample of vpp data

---
+++ scripts/exploreInitial.R
@@ -0,0 +1,16 @@
+f1 <- "/Volumes/hum-csafe/Research Projects/GREEN Grid/externalData/solarCity/2018-07-06-sampleData/RgAv_Sample_Data.csv.gz"
+
+f2 <- "/Volumes//hum-csafe/Research Projects/GREEN Grid/externalData/solarCity/2018-07-06-sampleData/Sample Monitoring Data.csv.gz"
+
+require(data.table)
+require(skimr)
+
+dt1 <- data.table::fread(f1)
+summary(dt1)
+head(dt1)
+skimr::skim(dt1)
+
+dt2 <- data.table::fread(f2)
+summary(dt2)
+head(dt2)
+skimr::skim(dt2)
